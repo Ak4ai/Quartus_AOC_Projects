3a5fcbd717a39fbca6455499606bf6a68956c473
Ak4ai <129908980+Ak4ai@users.noreply.github.com>
2024-07-26 19:18:59 -0300

    22

--- Aula2/simulation/qsim/work/@aula2/_primary.vhd
+++
@@ -1,8 +0,0 @@
-library verilog;
-use verilog.vl_types.all;
-entity Aula2 is
-    port(
-        pin_name2       : out    vl_logic;
-        pin_name1       : in     vl_logic
-    );
-end Aula2;
--- Aula2/simulation/qsim/work/@aula2_vlg_check_tst/_primary.vhd
+++
@@ -1,8 +0,0 @@
-library verilog;
-use verilog.vl_types.all;
-entity Aula2_vlg_check_tst is
-    port(
-        pin_name2       : in     vl_logic;
-        sampler_rx      : in     vl_logic
-    );
-end Aula2_vlg_check_tst;
