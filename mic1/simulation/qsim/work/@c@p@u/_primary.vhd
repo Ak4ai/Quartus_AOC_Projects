be2f38093c408a7f93c02bce43093c168419014f
Ak4ai <129908980+Ak4ai@users.noreply.github.com>
2024-11-08 15:26:42 -0300

    2.9

--- mic1/simulation/qsim/work/@c@p@u/_primary.vhd
+++ mic1/simulation/qsim/work/@c@p@u/_primary.vhd
@@ -2,14 +2,14 @@ library verilog;
 use verilog.vl_types.all;
 entity CPU is
     port(
-        MAR             : out    vl_logic_vector(31 downto 0);
+        ADDRESS         : out    vl_logic_vector(31 downto 0);
         CLOCK           : in     vl_logic;
         LOAD            : in     vl_logic;
         IN_MBR          : in     vl_logic_vector(7 downto 0);
-        IN_MDR          : in     vl_logic_vector(31 downto 0);
+        IN_MEM          : in     vl_logic_vector(31 downto 0);
         MIR             : in     vl_logic_vector(35 downto 0);
-        MBR_OUT         : out    vl_logic_vector(7 downto 0);
-        MDR_OUT         : out    vl_logic_vector(31 downto 0);
+        OUT_MBR         : out    vl_logic_vector(7 downto 0);
+        OUT_MEM         : out    vl_logic_vector(31 downto 0);
         PC              : out    vl_logic_vector(31 downto 0)
     );
 end CPU;
--- mic1/simulation/qsim/work/@c@p@u_vlg_check_tst/_primary.vhd
+++ mic1/simulation/qsim/work/@c@p@u_vlg_check_tst/_primary.vhd
@@ -2,9 +2,9 @@ library verilog;
 use verilog.vl_types.all;
 entity CPU_vlg_check_tst is
     port(
-        MAR             : in     vl_logic_vector(31 downto 0);
-        MBR_OUT         : in     vl_logic_vector(7 downto 0);
-        MDR_OUT         : in     vl_logic_vector(31 downto 0);
+        ADDRESS         : in     vl_logic_vector(31 downto 0);
+        OUT_MBR         : in     vl_logic_vector(7 downto 0);
+        OUT_MEM         : in     vl_logic_vector(31 downto 0);
         PC              : in     vl_logic_vector(31 downto 0);
         sampler_rx      : in     vl_logic
     );
